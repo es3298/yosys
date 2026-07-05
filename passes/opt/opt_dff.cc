7d48c223904dfb1167a0df3f43168e62ce336a22
es3298 <es3298@stanford.edu>
2026-07-05 11:38:45 -0700

    Name opt_dff helper logic from source cells

--- passes/opt/opt_dff.cc
+++ passes/opt/opt_dff.cc
@@ -74,31 +74,31 @@ struct OptDffWorker
 		return is_inactive(sig, pol) || (!opt.keepdc && sig == State::Sx);
 	}
 
-	SigSpec create_not(SigSpec a, bool is_fine) {
+	SigSpec create_not(SigSpec a, bool is_fine, Cell *cell) {
 		if (is_fine)
-			return module->NotGate(NEW_ID, a);
+			return module->NotGate(NEW_ID2_SUFFIX("not"), a, cell->get_src_attribute());
 		else
-			return module->Not(NEW_ID, a);
+			return module->Not(NEW_ID2_SUFFIX("not"), a, false, cell->get_src_attribute());
 	}
 
-	SigSpec create_and(SigSpec a, SigSpec b, bool is_fine) {
+	SigSpec create_and(SigSpec a, SigSpec b, bool is_fine, Cell *cell) {
 		if (is_fine)
-			return module->AndGate(NEW_ID, a, b);
+			return module->AndGate(NEW_ID2_SUFFIX("and"), a, b, cell->get_src_attribute());
 		else
-			return module->And(NEW_ID, a, b);
+			return module->And(NEW_ID2_SUFFIX("and"), a, b, false, cell->get_src_attribute());
 	}
 
-	void create_mux_to_output(SigSpec a, SigSpec b, SigSpec sel, SigSpec y, bool pol, bool is_fine) {
+	void create_mux_to_output(SigSpec a, SigSpec b, SigSpec sel, SigSpec y, bool pol, bool is_fine, Cell *cell) {
 		if (is_fine) {
 			if (pol)
-				module->addMuxGate(NEW_ID, a, b, sel, y);
+				module->addMuxGate(NEW_ID2_SUFFIX("mux"), a, b, sel, y, cell->get_src_attribute());
 			else
-				module->addMuxGate(NEW_ID, b, a, sel, y);
+				module->addMuxGate(NEW_ID2_SUFFIX("mux"), b, a, sel, y, cell->get_src_attribute());
 		} else {
 			if (pol)
-				module->addMux(NEW_ID, a, b, sel, y);
+				module->addMux(NEW_ID2_SUFFIX("mux"), a, b, sel, y, cell->get_src_attribute());
 			else
-				module->addMux(NEW_ID, b, a, sel, y);
+				module->addMux(NEW_ID2_SUFFIX("mux"), b, a, sel, y, cell->get_src_attribute());
 		}
 	}
 
@@ -215,7 +215,7 @@ struct OptDffWorker
 		return ret;
 	}
 
-	ctrl_t make_patterns_logic(const patterns_t &patterns, const ctrls_t &ctrls, bool make_gates)
+	ctrl_t make_patterns_logic(const patterns_t &patterns, const ctrls_t &ctrls, bool make_gates, Module *module, Cell *cell)
 	{
 		if (patterns.empty() && GetSize(ctrls) == 1)
 			return *ctrls.begin();
@@ -231,8 +231,8 @@ struct OptDffWorker
 				s2.append(it.second);
 			}
 
-			RTLIL::SigSpec y = module->addWire(NEW_ID);
-			RTLIL::Cell *c = module->addNe(NEW_ID, s1, s2, y);
+			RTLIL::SigSpec y = module->addWire(NEW_ID2_SUFFIX("pat_y"));
+			RTLIL::Cell *c = module->addNe(NEW_ID2_SUFFIX("pat_ne"), s1, s2, y, false, cell->get_src_attribute());
 			maybe_simplemap(c, make_gates);
 			or_input.append(y);
 		}
@@ -242,19 +242,19 @@ struct OptDffWorker
 			if (item.second)
 				or_input.append(item.first);
 			else
-				or_input.append(create_not(item.first, make_gates));
+				or_input.append(create_not(item.first, make_gates, cell));
 		}
 
 		if (GetSize(or_input) == 0) return ctrl_t(State::S1, true);
 		if (GetSize(or_input) == 1) return ctrl_t(or_input, true);
 
-		RTLIL::SigSpec y = module->addWire(NEW_ID);
-		RTLIL::Cell *c = module->addReduceAnd(NEW_ID, or_input, y);
+		RTLIL::SigSpec y = module->addWire(NEW_ID2_SUFFIX("pat_logic_y"));
+		RTLIL::Cell *c = module->addReduceAnd(NEW_ID2_SUFFIX("pat_logic_reduce_and"), or_input, y, false, cell->get_src_attribute());
 		maybe_simplemap(c, make_gates);
 		return ctrl_t(y, true);
 	}
 
-	ctrl_t combine_resets(const ctrls_t &ctrls, bool make_gates)
+	ctrl_t combine_resets(const ctrls_t &ctrls, bool make_gates, Module *module, Cell *cell)
 	{
 		if (GetSize(ctrls) == 1)
 			return *ctrls.begin();
@@ -269,13 +269,13 @@ struct OptDffWorker
 			if (item.second == final_pol)
 				or_input.append(item.first);
 			else
-				or_input.append(create_not(item.first, make_gates));
+				or_input.append(create_not(item.first, make_gates, cell));
 		}
 
-		RTLIL::SigSpec y = module->addWire(NEW_ID);
+		RTLIL::SigSpec y = module->addWire(NEW_ID2_SUFFIX("comb_rst_y"));
 		RTLIL::Cell *c = final_pol
-			? module->addReduceOr(NEW_ID, or_input, y)
-			: module->addReduceAnd(NEW_ID, or_input, y);
+			? module->addReduceOr(NEW_ID2_SUFFIX("comb_rst_reduce_or"), or_input, y, false, cell->get_src_attribute())
+			: module->addReduceAnd(NEW_ID2_SUFFIX("comb_rst_reduce_and"), or_input, y, false, cell->get_src_attribute());
 		maybe_simplemap(c, make_gates);
 		return ctrl_t(y, final_pol);
 	}
@@ -308,9 +308,9 @@ struct OptDffWorker
 				if (!ff.pol_clr)
 					module->connect(ff.sig_q[i], ff.sig_clr[i]);
 				else if (ff.is_fine)
-					module->addNotGate(NEW_ID, ff.sig_clr[i], ff.sig_q[i]);
+					module->addNotGate(NEW_ID2_SUFFIX("not"), ff.sig_clr[i], ff.sig_q[i], cell->get_src_attribute());
 				else
-					module->addNot(NEW_ID, ff.sig_clr[i], ff.sig_q[i]);
+					module->addNot(NEW_ID2_SUFFIX("not"), ff.sig_clr[i], ff.sig_q[i], false, cell->get_src_attribute());
 				log("Handling always-active SET at position %d on %s (%s) from module %s (changing to combinatorial circuit).\n",
 						i, cell, cell->type.unescape(), module);
 				sr_removed = true;
@@ -399,34 +399,34 @@ struct OptDffWorker
 			// ALOAD always active
 			log("Handling always-active async load on %s (%s) from module %s (changing to combinatorial circuit).\n",
 					cell, cell->type.unescape(), module);
-			ff.remove();
 
 			if (ff.has_sr) {
 				SigSpec tmp;
 				if (ff.is_fine) {
 					tmp = ff.pol_set
-						? module->MuxGate(NEW_ID, ff.sig_ad, State::S1, ff.sig_set)
-						: module->MuxGate(NEW_ID, State::S1, ff.sig_ad, ff.sig_set);
+						? module->MuxGate(NEW_ID2_SUFFIX("mux"), ff.sig_ad, State::S1, ff.sig_set, cell->get_src_attribute())
+						: module->MuxGate(NEW_ID2_SUFFIX("mux"), State::S1, ff.sig_ad, ff.sig_set, cell->get_src_attribute());
 
 					if (ff.pol_clr)
-						module->addMuxGate(NEW_ID, tmp, State::S0, ff.sig_clr, ff.sig_q);
+						module->addMuxGate(NEW_ID2_SUFFIX("mux"), tmp, State::S0, ff.sig_clr, ff.sig_q, cell->get_src_attribute());
 					else
-						module->addMuxGate(NEW_ID, State::S0, tmp, ff.sig_clr, ff.sig_q);
+						module->addMuxGate(NEW_ID2_SUFFIX("mux"), State::S0, tmp, ff.sig_clr, ff.sig_q, cell->get_src_attribute());
 				} else {
 					tmp = ff.pol_set
-						? module->Or(NEW_ID, ff.sig_ad, ff.sig_set)
-						: module->Or(NEW_ID, ff.sig_ad, module->Not(NEW_ID, ff.sig_set));
+						? module->Or(NEW_ID2_SUFFIX("or"), ff.sig_ad, ff.sig_set, false, cell->get_src_attribute())
+						: module->Or(NEW_ID2_SUFFIX("or"), ff.sig_ad, module->Not(NEW_ID2_SUFFIX("not"), ff.sig_set, false, cell->get_src_attribute()), false, cell->get_src_attribute());
 
 					if (ff.pol_clr)
-						module->addAnd(NEW_ID, tmp, module->Not(NEW_ID, ff.sig_clr), ff.sig_q);
+						module->addAnd(NEW_ID2_SUFFIX("and"), tmp, module->Not(NEW_ID2_SUFFIX("not"), ff.sig_clr, false, cell->get_src_attribute()), ff.sig_q, false, cell->get_src_attribute());
 					else
-						module->addAnd(NEW_ID, tmp, ff.sig_clr, ff.sig_q);
+						module->addAnd(NEW_ID2_SUFFIX("and"), tmp, ff.sig_clr, ff.sig_q, false, cell->get_src_attribute());
 				}
 			} else if (ff.has_arst) {
-				create_mux_to_output(ff.sig_ad, ff.val_arst, ff.sig_arst, ff.sig_q, ff.pol_arst, ff.is_fine);
+				create_mux_to_output(ff.sig_ad, ff.val_arst, ff.sig_arst, ff.sig_q, ff.pol_arst, ff.is_fine, cell);
 			} else {
 				module->connect(ff.sig_q, ff.sig_ad);
 			}
+			ff.remove();
 			return true;
 		}
 
@@ -534,9 +534,9 @@ struct OptDffWorker
 			log("Handling D = Q on %s (%s) from module %s (conecting SRST instead).\n",
 					cell, cell->type.unescape(), module);
 			if (ff.has_ce && ff.ce_over_srst) {
-				SigSpec ce = ff.pol_ce ? ff.sig_ce : create_not(ff.sig_ce, ff.is_fine);
-				SigSpec srst = ff.pol_srst ? ff.sig_srst : create_not(ff.sig_srst, ff.is_fine);
-				ff.sig_ce = create_and(ce, srst, ff.is_fine);
+				SigSpec ce = ff.pol_ce ? ff.sig_ce : create_not(ff.sig_ce, ff.is_fine, cell);
+				SigSpec srst = ff.pol_srst ? ff.sig_srst : create_not(ff.sig_srst, ff.is_fine, cell);
+				ff.sig_ce = create_and(ce, srst, ff.is_fine, cell);
 				ff.pol_ce = true;
 			} else {
 				ff.pol_ce = ff.pol_srst;
@@ -615,7 +615,7 @@ struct OptDffWorker
 
 			new_ff.val_srst = new_val_srst_builder.build();
 
-			ctrl_t srst = combine_resets(it.first, ff.is_fine);
+			ctrl_t srst = combine_resets(it.first, ff.is_fine, module, cell);
 			new_ff.has_srst = true;
 			new_ff.sig_srst = srst.first;
 			new_ff.pol_srst = srst.second;
@@ -623,6 +623,7 @@ struct OptDffWorker
 				new_ff.ce_over_srst = true;
 
 			Cell *new_cell = new_ff.emit();
+			module->swap_names(cell, new_cell);
 			if (new_cell)
 				dff_cells.push_back(new_cell);
 
@@ -689,7 +690,7 @@ struct OptDffWorker
 
 		for (auto &it : groups) {
 			FfData new_ff = ff.slice(it.second);
-			ctrl_t en = make_patterns_logic(it.first.first, it.first.second, ff.is_fine);
+			ctrl_t en = make_patterns_logic(it.first.first, it.first.second, ff.is_fine, module, cell);
 
 			new_ff.has_ce = true;
 			new_ff.sig_ce = en.first;
@@ -697,6 +698,7 @@ struct OptDffWorker
 			new_ff.ce_over_srst = false;
 
 			Cell *new_cell = new_ff.emit();
+			module->swap_names(cell, new_cell);
 			if (new_cell)
 				dff_cells.push_back(new_cell);
 
@@ -767,7 +769,7 @@ struct OptDffWorker
 				optimize_d_equals_q(ff, cell, changed);
 
 			if (ff.has_aload && !ff.has_clk && ff.sig_ad == ff.sig_q) {
-				log("Handling AD = Q on %s (%s) from module %s (removing async load path).\n",
+				log_debug("Handling AD = Q on %s (%s) from module %s (removing async load path).\n",
 						cell, cell->type.unescape(), module);
 				ff.has_aload = false;
 				changed = true;
